7b18d589f686ddd2554159ab82b3fd312f367948
spark <fabricio.jacob@outlook.com>
2025-06-17 22:20:21 -0300

    init project.

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -0,0 +1,38 @@
+package com.algaworks.algasensors.device.management.api.client.impl;
+
+import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
+import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
+import io.hypersistence.tsid.TSID;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class SensorMonitoringClientImpl implements SensorMonitoringClient {
+
+    private final RestClient restClient;
+
+    public SensorMonitoringClientImpl(RestClient.Builder builder) {
+        this.restClient = builder.baseUrl("http://localhost:8082")
+                .defaultStatusHandler(HttpStatusCode::isError, ((request, response) -> {
+                    throw new SensorMonitoringClientBadGatewayException();
+                }))
+                .build();
+    }
+
+    @Override
+    public void enableMonitoring(TSID sensorId) {
+        restClient.put()
+                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    @Override
+    public void disableMonitoring(TSID sensorId) {
+        restClient.delete()
+                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
